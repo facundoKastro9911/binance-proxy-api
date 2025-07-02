4035163a93719acf98eb9fb715e85442a5afc99a
facundoKastro9911 <facundo.sanchezcastro1999@gmail.com>
2025-07-02 19:13:57 -0300

    Update [symbol].js

--- [symbol].js
+++ [symbol].js
@@ -2,11 +2,15 @@ export default async function handler(req, res) {
   const { symbol } = req.query;
 
   try {
-    const response = await fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`);
+    const response = await fetch(`https://api.binance.com/api/v3/ticker/bookTicker?symbol=${symbol}`);
+    
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}`);
+    }
+
     const data = await response.json();
-    res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching Binance data' });
+    res.status(500).json({ error: error.message });
   }
 }
