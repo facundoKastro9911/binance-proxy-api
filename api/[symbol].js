bf4c56848716a4a05e6fe0b42f608d4a60e4f4fc
facundoKastro9911 <facundo.sanchezcastro1999@gmail.com>
2025-07-02 19:45:04 -0300

    Update [symbol].js

--- api/[symbol].js
+++ api/[symbol].js
@@ -1,16 +1,18 @@
 export default async function handler(req, res) {
-  const {
-    query: { symbol },
-  } = req;
+  const { symbol } = req.query;
 
   try {
     const response = await fetch(`https://api.binance.com/api/v3/ticker/bookTicker?symbol=${symbol.toUpperCase()}`);
+    
     if (!response.ok) {
-      throw new Error(`Error en la solicitud: ${response.statusText}`);
+      return res.status(response.status).json({ error: "Error en Binance", status: response.status });
     }
+
     const data = await response.json();
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json(data);
+
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: "Error interno", message: error.message });
   }
 }
